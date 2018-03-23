3985b0cd5c28d90e52ce64c572941aff14f6820d
XSP16SCM97M <xma39@hawk.iit.edu>
2018-03-22 23:06:31 -0500

    stage 1

---
+++ src/Main.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import { WrappedRegister } from './Register';
+
+export class Main extends React.Component {
+    render() {
+        return (
+            <section className="main">
+                <WrappedRegister/>
+            </section>
+        );
+    }
+}
